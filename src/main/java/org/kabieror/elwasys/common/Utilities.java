45be89bf97b9cf2a173eaf46b92711022f1e892a
Oliver Kabierschke <code@oliver-kabierschke.de>
2019-01-27 17:33:04 +0100

    Release

--- src/main/java/org/kabieror/elwasys/common/Utilities.java
+++ src/main/java/org/kabieror/elwasys/common/Utilities.java
@@ -22,7 +22,7 @@ import java.util.Iterator;
  */
 public class Utilities {
 
-    public static final String APP_VERSION = "0.3.6-SNAPSHOT";
+    public static final String APP_VERSION = "0.3.6";
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
